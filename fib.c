ab26544a74a9db1d084f7fcd4afc62faa42076ab
Josel Catalan <catalanjosel123@gmail.com>
2025-12-26 09:44:30 +0800

    feat: implement memoization using array

--- fib.c
+++ fib.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long int fib(long int n) {
+#define MAP_SIZE 100
+
+unsigned long long map[MAP_SIZE];
+
+void init_map() {
+  for (int i = 0; i < MAP_SIZE; i++) {
+    map[i] = -1;
+  }
+}
+
+unsigned long long fib(unsigned long long n) {
   if (n <= 1) {
     return n;
   }
 
-  return fib(n - 1) + fib(n - 2);
+  if (map[n] != -1) {
+    return map[n];
+  }
+
+  map[n] = fib(n - 1) + fib(n - 2);
+  return map[n];
 }
 
-int main(int argc, char* argv[]) {
-  long int result = fib(atoi(argv[1]));
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Usage: %s <n>\n", argv[0]);
+    return -1;
+  }
 
-  printf("%ld\n", result);
+  init_map();
+
+  long long result = fib(atoi(argv[1]));
+
+  printf("%lld\n", result);
 
   return 0;
 }
-
